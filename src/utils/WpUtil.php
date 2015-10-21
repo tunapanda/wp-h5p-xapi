a024c883014411a43fd01468d0b1633f38fdc60a
Mikael Lindqvist <li.mikael@gmail.com>
2015-10-21 11:11:31 +0300

    check for class re-declaration

--- src/utils/WpUtil.php
+++ src/utils/WpUtil.php
@@ -5,30 +5,32 @@
 	/**
 	 * Wordpress utils.
 	 */
-	class WpUtil {
+	if (!class_exists("h5pxapi\\WpUtil")) {
+		class WpUtil {
 
-		/**
-		 * Bootstrap from inside a plugin.
-		 */
-		public static function getWpLoadPath() {
-			$path=$_SERVER['SCRIPT_FILENAME'];
+			/**
+			 * Bootstrap from inside a plugin.
+			 */
+			public static function getWpLoadPath() {
+				$path=$_SERVER['SCRIPT_FILENAME'];
 
-			for ($i=0; $i<4; $i++)
-				$path=dirname($path);
+				for ($i=0; $i<4; $i++)
+					$path=dirname($path);
 
-			return $path."/wp-load.php";
-		}
+				return $path."/wp-load.php";
+			}
 
-		/**
-		 * Create a PDO object that is compatible with the current
-		 * wordpress install.
-		 */
-		public static function getCompatiblePdo() {
-			static $pdo;
+			/**
+			 * Create a PDO object that is compatible with the current
+			 * wordpress install.
+			 */
+			public static function getCompatiblePdo() {
+				static $pdo;
 
-			if (!$pdo)
-				$pdo=new PDO("mysql:host=".DB_HOST.";dbname=".DB_NAME,DB_USER,DB_PASSWORD);
+				if (!$pdo)
+					$pdo=new PDO("mysql:host=".DB_HOST.";dbname=".DB_NAME,DB_USER,DB_PASSWORD);
 
-			return $pdo;
+				return $pdo;
+			}
 		}
 	}
